fa1efc671c32ae0366ba8a82382113fa7b4da0a8
nicole <nicole732470@gmail.com>
2026-04-17 16:21:22 -0500

    Implement full CRUD for Products and PriceRecords

--- app/controllers/price_records_controller.rb
+++ app/controllers/price_records_controller.rb
@@ -1,13 +1,53 @@
 class PriceRecordsController < ApplicationController
+  before_action :set_price_record, only: [:show, :edit, :update, :destroy]
+
   def index
+    @price_records = PriceRecord.all.includes(:product).order(recorded_at: :desc)
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @product = Product.find(params[:product_id])
+    @price_record = @product.price_records.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @price_record = @product.price_records.new(price_record_params)
+    @price_record.recorded_at ||= Time.current
+    if @price_record.save
+      redirect_to @product, notice: 'Price record added successfully!'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @product = @price_record.product
+  end
+
+  def update
+    if @price_record.update(price_record_params)
+      redirect_to @price_record, notice: 'Price record updated successfully!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = @price_record.product
+    @price_record.destroy
+    redirect_to product, notice: 'Price record deleted.'
+  end
+
+  private
+
+  def set_price_record
+    @price_record = PriceRecord.find(params[:id])
+  end
+
+  def price_record_params
+    params.require(:price_record).permit(:price, :store_name, :url, :recorded_at, :notes)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,52 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @products = Product.all.includes(:price_records)
+    if params[:search].present?
+      @products = @products.where("name LIKE ? OR category LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
   end
 
   def show
+    @price_records = @product.price_records.order(recorded_at: :desc)
   end
 
   def new
+    @product = Product.new
   end
 
-  def edit
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product added successfully!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Product updated successfully!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_url, notice: 'Product deleted.'
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :category, :description, :image_url)
   end
 end
