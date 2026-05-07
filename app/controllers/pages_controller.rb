8aa6151edbfc3f929f7cde6fa4660d53fdadfec5
andrewhxd <andrewhxue@gmail.com>
2026-05-07 10:34:00 -0500

    Add About, Privacy, and Terms pages with footer links

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  allow_unauthenticated_access
+
+  def about; end
+  def privacy; end
+  def terms; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   end
   resources :price_records, only: [ :index, :show, :edit, :update, :destroy ]
   get "budgetplanner", to: "budget_planner#index", as: :budget_planner
+
+  get "about",   to: "pages#about",   as: :about
+  get "privacy", to: "pages#privacy", as: :privacy
+  get "terms",   to: "pages#terms",   as: :terms
 end
